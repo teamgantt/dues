1c667f64346c73708710a3bc80369b4978be576f
John Correlli <john@teamgantt.com>
2021-12-21 15:28:42 -0500

    chore(composer): update php-cs-fixer and phpstan

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,12 +3,14 @@
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__);
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+$config
     ->setRules([
         '@PSR1' => true,
         '@PSR2' => true,
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
+
+return $config;
